0360f98b915f95678925fd8a42b36e243550b4d0
Micah Kravia <micah.kravia@my.jcu.edu.au>
2015-05-26 19:00:30 +1000

    Added inc_leftSidebar.php
    Contains html for the left sidebar, containing information on
    membership.

---
+++ TCMC_14_Milestone_2/inc_leftSidebar.php
@@ -0,0 +1,8 @@
+<div class="sidebar">
+	<p><var>Are you new  to Townsville? Do you love music with a passion</var>?</p>
+    <p>Then come and join the Townsville Community Music Center <em>NOW!</em> You can become a Registered user for <em>FREE!</em></p>
+	<p>By becoming  a <strong>Registered User</strong>, you can submit and edit notices for your favorite music band or events.</p>
+	<p>Once you become a registered member you can choose to upgrade to being a <strong>Paid Member</strong>.</p>
+	<p>By becoming a <strong>Paid Member</strong> can submit/edit notices and artists.</p>
+	<p>No matter what your age or taste in music, the Townsville Community Music Centre has somthing for you.</p>
+</div>
