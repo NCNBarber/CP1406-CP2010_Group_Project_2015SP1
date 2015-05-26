3ad7d44df5f0f73f282b2875378d9e6786ad31a4
lara Johnstone <lara.johnstone@my.jcu.edu.au>
2015-05-26 19:11:37 +1000

    fix footer div

--- TCMC_14_Milestone_2/inc_footer.php
+++ TCMC_14_Milestone_2/inc_footer.php
@@ -1,23 +1,24 @@
-<p><strong>Contact Us at :</strong></p>
-<p>Phone<strong> </strong></p>
-<p><strong> 07 4724 2086 </strong></p>
-<p>&nbsp;</p>
-<p>Mobile<strong>   </strong></p>
-<p><strong> 0402 255 182</strong> </p>
-<p>&nbsp;</p>
-<p>Email </p>
-<p><strong>admin@townsvillemusic.org.au</strong></p>
-<p>&nbsp;</p>
-<p>Mailing Address<strong>  </strong></p>
-<p><strong>PO Box 1006, </strong></p>
-<p><strong>Townsville, Qld 4810</strong><br />
-</p>
-<p>Location</p>
-<p><strong>Townsville Civic Theatre,</strong></p>
-<p><strong> 41 Boundary Street, </strong><strong>Townsville,</strong></p>
-<p><strong>Qld 4810</strong> </p>
-<p>&nbsp;</p>
-<p><img src="images/Townsville-City-Council-.jpg" width="318" height="400" alt="Townsville City Council logo" /></p>
-<p>&nbsp;</p>
-<img src="images/QldGovLogo.jpg" width="344" height="400" alt="Queensland Government" />
-<p>&nbsp;</p>
+<div id="footer">	
+    <p><strong>Contact Us at :</strong></p>
+	<p>Phone<strong> </strong></p>
+	<p><strong> 07 4724 2086 </strong></p>
+	<p>&nbsp;</p>
+	<p>Mobile<strong>   </strong></p>
+	<p><strong> 0402 255 182</strong> </p>
+	<p>&nbsp;</p>
+	<p>Email </p>
+	<p><strong>admin@townsvillemusic.org.au</strong></p>
+	<p>&nbsp;</p>
+	<p>Mailing Address<strong>  </strong></p>
+	<p><strong>PO Box 1006, </strong></p>
+	<p><strong>Townsville, Qld 4810</strong><br /></p>
+	<p>Location</p>
+	<p><strong>Townsville Civic Theatre,</strong></p>
+	<p><strong> 41 Boundary Street, </strong><strong>Townsville,</strong></p>
+	<p><strong>Qld 4810</strong> </p>
+	<p>&nbsp;</p>
+	<p><img src="images/Townsville-City-Council-.jpg" width="318" height="400" alt="Townsville City Council logo" /></p>
+	<p>&nbsp;</p>
+	<img src="images/QldGovLogo.jpg" width="344" height="400" alt="Queensland Government" />
+	<p>&nbsp;</p>
+</div>
