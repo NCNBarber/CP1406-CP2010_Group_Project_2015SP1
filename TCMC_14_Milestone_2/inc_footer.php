b8e9d964289d54ea94f97dea43100453f9b06e1d
lara Johnstone <lara.johnstone@my.jcu.edu.au>
2015-05-27 14:50:12 +1000

    Added inc_sponsors.php
    Now with CHEESECAKE!

--- TCMC_14_Milestone_2/inc_footer.php
+++ TCMC_14_Milestone_2/inc_footer.php
@@ -1,24 +1,18 @@
 <div id="footer">	
-    <p><strong>Contact Us at :</strong></p>
-	<p>Phone<strong> </strong></p>
-	<p><strong> 07 4724 2086 </strong></p>
-	<p>&nbsp;</p>
-	<p>Mobile<strong>   </strong></p>
-	<p><strong> 0402 255 182</strong> </p>
-	<p>&nbsp;</p>
-	<p>Email </p>
-	<p><strong>admin@townsvillemusic.org.au</strong></p>
-	<p>&nbsp;</p>
-	<p>Mailing Address<strong>  </strong></p>
-	<p><strong>PO Box 1006, </strong></p>
-	<p><strong>Townsville, Qld 4810</strong><br /></p>
-	<p>Location</p>
-	<p><strong>Townsville Civic Theatre,</strong></p>
-	<p><strong> 41 Boundary Street, </strong><strong>Townsville,</strong></p>
-	<p><strong>Qld 4810</strong> </p>
-	<p>&nbsp;</p>
-	<p><img src="images/Townsville-City-Council-.jpg" width="318" height="400" alt="Townsville City Council logo" /></p>
-	<p>&nbsp;</p>
-	<img src="images/QldGovLogo.jpg" width="344" height="400" alt="Queensland Government" />
-	<p>&nbsp;</p>
+    <div class="footerContentContainerLeft">
+    	<p><strong>Contact us at:</strong></p>
+		<p><strong>Phone:</strong> 07 4724 2086<br/>
+		<strong>Mobile:</strong> 0402 255 182<br/>
+		<strong>Email:</strong> admin@townsvillemusic.org.au<br/>
+		<strong>Mailing Address:</strong> PO Box 1006, Townsville, Qld 4810<br/>
+		<strong>Location:</strong> Townsville Civic Theatre, 41 Boundary Street, Townsville, Qld 4810</p>
+	</div>
+    <div class="footerContentContainerRight">
+        <img src="images/QldGovLogo.jpg" width="86" height="100" alt="Queensland Government Logo"/>
+	</div>
+    <div class="footerContentContainerRight">
+		<img src="images/Townsville-City-Council-.jpg" width="79.5" height="100" alt="Townsville City Council Logo"/>
+	</div>
+    <div class="floatClear">
+    </div>
 </div>
