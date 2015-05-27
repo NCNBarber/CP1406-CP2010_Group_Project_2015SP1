19572ca3b1ee350c177c395f81007614175069aa
lara Johnstone <lara.johnstone@my.jcu.edu.au>
2015-05-27 16:42:41 +1000

    edit

--- TCMC_14_Milestone_2/inc_header.php
+++ TCMC_14_Milestone_2/inc_header.php
@@ -1,3 +1,22 @@
 <div id="header">
 	<img src="images/logo.png" alt="Townsville Community Music Centre" class="alignedCentre"/>
-</div>
+    
+    <form>
+    <fieldset>
+      <h1>
+        <legend>Sign-in</legend>
+      </h1>
+      <p>User Name:<br>
+        <input type="text" name="name">
+        <br>
+        Password:<br>
+        <input type="text" name="Phone number">
+        <br>
+        
+        <br><br>
+        <input type="submit" value="Submit">
+      </p>
+    </fieldset>
+  </form>
+    
+   </div> 
--- TCMC_14_Milestone_2/inc_sponsors.php
+++ TCMC_14_Milestone_2/inc_sponsors.php
@@ -3,26 +3,23 @@
 <head>
 <meta charset="utf-8">
 <title>Template Page</title>
-<link href="mainstyles.css" rel="stylesheet" type="text/css">
+<link href="styles/mainstyles.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 <div id="container">
-    	<?php include("inc_header.php"); ?>
-        <?php include("inc_navigation.php"); ?>
-        <div class="sidebar">
-        	<p>Nam eu ex mi. Integer nec leo a lacus malesuada tempor. Nullam semper lacus eget quam commodo maximus. Suspendisse et vehicula nunc. Sed nec dui ultrices, semper arcu a, egestas arcu. Phasellus a sapien ac felis finibus vestibulum. Aenean finibus pellentesque ipsum, a tempor tellus faucibus accumsan. Pellentesque imperdiet id nisi ac gravida. Nulla eget elit et arcu aliquam bibendum. Nunc nec aliquet velit, non mattis lacus.</p>
-            <p>Vestibulum ut tincidunt nisi, ac hendrerit nulla. Pellentesque sodales mattis nisl ac euismod. Phasellus non feugiat urna. Pellentesque quis mauris auctor, consectetur orci in, vestibulum purus. Pellentesque tincidunt purus et ante fringilla auctor. Cras sit amet arcu leo. Integer lectus diam, mollis et sem vel, faucibus pharetra felis. Nullam luctus justo a commodo fermentum.</p>
-        </div>
-        <div id="contentContainer">
-       	  <p><img src="images/Townsville-City-Council-.jpg" width="318" height="400" alt="townsville city council"></p>
-        	<p>Townsville City Council <br>
-        	  The Council's Partnerships and Sponsorships scheme provides vital core funding which enables us to maintain the administrative base for all our other activities, and also provides the premises which house our office space. <br>
+  <?php include("inc_navigation.php"); ?>
+        <?php include("inc_leftSidebar.php"); ?>
+  <div id="contentContainer">
+       	  <p><img src="images/Townsville-City-Council-.jpg" alt="townsville city council" width="239" height="300" class="alignedCentre"></p>
+        	<p align="center"><strong>Townsville City Council</strong></p>
+        	<p align="left">The Council's Partnerships and Sponsorships scheme provides vital core funding which enables us to maintain the administrative base for all our other activities, and also provides the premises which house our office space. <br>
        	    The Council also assists with the performance venues for our concerts and workshop.</p>
-        	<p><img src="images/Gambling-Community-Benefit.jpg" width="644" height="400" alt=" Gambling Community Benefit "></p>
-        	<p>Queensland Government <br>
-The Gambling Community Benefit Fund has assisted us to obtain office equipment and sound and lighting equipment for our productions </p> 
-        </div>
+        	<p>&nbsp;</p>
+        	<p><img src="images/Gambling-Community-Benefit.jpg" alt=" Gambling Community Benefit " width="300" height="188" class="alignedCentre"></p>
+   	<p align="center"><strong>Queensland Government</strong></p>
+   	<p align="left">The Gambling Community Benefit Fund has assisted us to obtain office equipment and sound and lighting equipment for our productions.</p>
+  </div>
         <div class="sidebar">
         	<p>Nam eu ex mi. Integer nec leo a lacus malesuada tempor. Nullam semper lacus eget quam commodo maximus. Suspendisse et vehicula nunc. Sed nec dui ultrices, semper arcu a, egestas arcu. Phasellus a sapien ac felis finibus vestibulum. Aenean finibus pellentesque ipsum, a tempor tellus faucibus accumsan. Pellentesque imperdiet id nisi ac gravida. Nulla eget elit et arcu aliquam bibendum. Nunc nec aliquet velit, non mattis lacus.</p>
             <p>Vestibulum ut tincidunt nisi, ac hendrerit nulla. Pellentesque sodales mattis nisl ac euismod. Phasellus non feugiat urna. Pellentesque quis mauris auctor, consectetur orci in, vestibulum purus. Pellentesque tincidunt purus et ante fringilla auctor. Cras sit amet arcu leo. Integer lectus diam, mollis et sem vel, faucibus pharetra felis. Nullam luctus justo a commodo fermentum.</p>
