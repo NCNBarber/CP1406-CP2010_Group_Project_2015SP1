0dd2043d254225f0263369409eed517211cc5398
lara Johnstone <lara.johnstone@my.jcu.edu.au>
2015-05-29 00:13:24 +1000

    edit the home.php

--- TCMC_14_Milestone_2/inc_home.php
+++ TCMC_14_Milestone_2/inc_home.php
@@ -22,12 +22,21 @@
 	<h2>Information of Interest</h2>
     <p>Townsville has a population of about 200,000 and is growing at about 1 suburb per year, so there is a lot of musical activity. All private schools and most government schools have music teachers. The larger private schools are Townsville Grammar, the Cathedral School and Ryan Catholic College. The larger public high schools are Kirwan and Pimlico.</p>
     <p>With the support of the Townsville City Council, we work from an office in the Civic Theatre building. (Take the lift near the ticket office to Level 2)</p>
-	<p>"Normal" office hours - 9.30 -2.30 Monday - Wednesday Any other time just call 0402 255 182</p>
-	<p>Contact Details: Phone 07 4724 2086<br>Mobile 0402 255 182 </p>
-    <p>Postal Address PO Box 1006, Townsville, Qld 4810 Address Townsville Civic Theatre, 41 Boundary Street, Townsville, Qld 4810</p>
+	<p>"Normal" office hours - 9.30 -2.30 Monday - Wednesday Any other time just call the number below</p>
+	<p>Contact Details:</p>
+	<p> Phone: (07) 4724 2086<br>
+	Mobile: 0402 255 182 </p>
+    <p>Postal Address PO Box 1006, Townsville, Qld 4810 </p>
+    <p>Address Townsville Civic Theatre, 41 Boundary Street, Townsville, Qld 4810</p>
     <p>Are you new to Townsville? General information about Townsville is available at <a href="http://www.townsville.qld.gov.au/townsville/infocentre/Pages/default.aspx">http://www.townsville.qld.gov.au/townsville/infocentre/Pages/default.aspx</a></p>
 	<p>Some local businesses also employ or assist music teachers. Try these - heather@thekeyboardshop.com.au and <a href="http://www.artiesmusiconline.com.au">www.artiesmusiconline.com.au/</a></p>
-	<p>Music Teachers Association of Qld is a good source of information for local music teachers. The Townsville Branch contact is the Secretary (Ms Margery Jorgensen) Phone (07) 47790382 Email: mjo11750@bigpond.net.au Another active teacher's organisation is the Kodaly Music Education Institute of Australia - <a href="http://www.kodaly.org.au">www.kodaly.org.au/</a> Information on their Townsville Chapter can be found at - <a href="http://webapps.townsville.qld.gov.au/CommunityDirectory/Organisation/OrganisationDetails/1185">http://webapps.townsville.qld.gov.au/CommunityDirectory/Organisation/OrganisationDetails/1185</a></p>
-	<p>Busking is permitted at several public spaces around the city with a Buskers Permit from the city council - phone 4727 9680. There is no age limit, but buskers 16 and under will need to be accompanied by a parent/guardian.</p>
+	<p>Music Teachers Association of Qld is a good source of information for local music teachers. The Townsville Branch contact is the Secretary</p>
+	<p> (Ms Margery Jorgensen) </p>
+	<p>Phone: (07) 47790382</p>
+	<p> Email: mjo11750@bigpond.net.au</p>
+	<p> Another active teacher's organisation is the Kodaly Music Education Institute of Australia - <a href="http://www.kodaly.org.au">www.kodaly.org.au/</a> </p>
+	<p>Information on their Townsville Chapter can be found at - <a href="http://webapps.townsville.qld.gov.au/CommunityDirectory/Organisation/OrganisationDetails/1185">http://webapps.townsville.qld.gov.au/CommunityDirectory/Organisation/OrganisationDetails/1185</a></p>
+	<p>Busking is permitted at several public spaces around the city with a Buskers Permit from the city council - phone (07) 4727 9680.</p>
+	<p> There is no age limit, but buskers 16 and under will need to be accompanied by a parent/guardian.</p>
     </div>
 </div>
