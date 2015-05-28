e2afdd62ce7078f02789372278d5ae6abbda3fd8
lara Johnstone <lara.johnstone@my.jcu.edu.au>
2015-05-29 00:22:01 +1000

    edit sponsor

--- TCMC_14_Milestone_2/inc_sponsors.php
+++ TCMC_14_Milestone_2/inc_sponsors.php
@@ -1,6 +1,9 @@
 <div id="contentContainer">
-	<a href="http://www.townsville.qld.gov.au/Pages/default.aspx"><img  class="alignedCentre" src="images/Townsville-City-Council-.jpg" alt="townsville city council" width="239" height="300"></a>
-	<p class="centreText"><strong>Townsville City Council</strong></p>
+	<p>The Music Centre receives vital support from the sponsors featured on this page
+Our music concerts are well-publicised and well-attended and sponsors enjoy brand exposure throughout the Arts community. We welcome sponsorship or assistance in any form.
+</p>
+	<a href="http://www.townsville.qld.gov.au/Pages/default.aspx"><img  class="alignedCentre" src="images/Townsville-City-Council-.jpg" alt="townsville city council" width="184" height="230"></a>
+  <p class="centreText"><strong>Townsville City Council</strong></p>
 	<p class="centreText">The Council's Partnerships and Sponsorships scheme provides vital core funding which enables us to maintain the administrative base for all our other activities, and also provides the premises which house our office space. <br>
        	    The Council also assists with the performance venues for our concerts and workshop.</p>
     <a href="https://www.business.qld.gov.au/industry/liquor-gaming/gambling-community-benefit-fund"><img src="images/Gambling-Community-Benefit.jpg" alt=" Gambling Community Benefit " width="300" height="188" class="alignedCentre"></a>
