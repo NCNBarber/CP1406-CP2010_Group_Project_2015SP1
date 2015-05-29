17f653dd83c17df5236f325dd343a96588bd7a85
lara Johnstone <lara.johnstone@my.jcu.edu.au>
2015-05-29 13:55:34 +1000

    about us php

---
+++ TCMC_14_Milestone_2/inc_aboutus.php
@@ -0,0 +1,94 @@
+<div id="contentContainer">
+	<p>Based in Townsville, North Qld, the Music Centre presents concerts and workshops throughout the year, in a diverse range of genres including classical, jazz, folk, blues, world and contemporary music, featuring touring artists and locally-based professional and emerging artists.</p>
+	<a href="http://www.townsville.qld.gov.au/Pages/default.aspx"><img src="images/old-music.jpg" alt="picture music books" width="500" height="237" align="middle" /></a>
+  <p align="center">Looking Back - The first 30 years! <br />
+    <br />
+    <br clear="all" />
+    <strong>The Townsville Community Music Centre celebrated its 30th year in 2013. </strong><br />
+    <br />
+    It has been constantly changing over the years to keep up to date with the musical tastes and needs of the Townsville community. As part of the relocation of the Music Centre to the Civic Theatre, Bronia Renison and Jean Dartnall, both librarians, have assessed the old collection of sheet music, books and recorded music which the centre has been storing, unused, for many years. Sometimes older things have to be discarded to make way for the new, but the Music Centre is aware that older material may still have value. <br />
+    The National Library of Australia has an online catalogue (TROVE) that lists not only its own holdings but also information about items held by many other libraries around Australia. Using this catalogue Bronia and Jean have identified at least 150 items of music that are not held by any of the country's major libraries. These items have been donated to the National Library to include in their collection and thus made available to all historians and musicians.<br />
+    <br />
+  Also discovered in the old collection were some pieces relevant to North Queensland. Local musicians performed these at a musical social afternoon on Sunday April 21st in C2 at the Civic Theatre. The remaining sheet music, books and CDs were put on display and distributed free of charge to the local music community.</p>
+  
+  <p align="center"><strong>A Brief History</strong><br />
+by Jean Dartnall<br />
+<br />
+  </p>
+  <center>
+    <table>
+      <tbody valign="top">
+        <tr>
+          <td><strong>1983</strong></td>
+          <td> </td>
+          <td>The Townsville Community Music Centre was established at a public meeting on May 24th. Then Deputy Mayor, Ken McElligott, opened the meeting and presented a cheque for $50 from the City Council to cover initial petty cash, the Centre's first funds. At the meeting an executive committee was formed. Over the next few weeks that committee drafted a constitution which was endorsed at another public meeting on June 15th. Fred Thompson was Chairman from September 1983, with Gordon Dean as his Deputy and Jan Eagleton as Secretary.<br /></td>
+        </tr>
+        <tr>
+          <td><strong>1984</strong></td>
+          <td>-</td>
+          <td>Mira Henderson, who had extensive experience in community music work in England, was the first staff member. (Her position was Acting Director because she was only in Townsville for a brief spell.) She instigated a series of classes starting in 1984 using the skills of people living in Townsville to enhance and encourage music learners of all ages.</td>
+        </tr>
+        <tr>
+          <td><strong>1985</strong></td>
+          <td>-</td>
+          <td>Kirsty Veron was appointed as the first Director. Kirsty was very interested in teaching music to children and had worked with Mira in the 1983 classes. She held this position until 1988 leading the new organisation into a strong position in the Townsville arts community.<br /></td>
+        </tr>
+        <tr>
+          <td><strong>1985</strong></td>
+          <td>-</td>
+          <td>The Music Centre produced Benjamin Britten's Noyes Fludde in St James Anglican Cathedral. This involved hundreds of children and some of Townsville's best known (and loved) musicians. It was produced by Rachel Berker (now Rachel Matthews) and involved Bernie Lannigan and Sandra Voss in the main parts rehearsed by Bill Williams; Donna McMahon and Jenny Carr rehearsing the strings and recorders and guest conductor Donald Hollier. This was by the no means the only major production mounted by the Music Centre but was probably the largest.<br /></td>
+        </tr>
+        <tr>
+          <td><strong>1987</strong></td>
+          <td>-</td>
+          <td>This was the year of the first fund raising dinner. These dinners became a tradition as they were a wonderful mixture of good food, good wine and good music. The first ones were held at the home of Paul and June Tonnoir in North Ward. Later they moved to the Bishop's Lodge as guests of the Anglican Bishop. Attendees would often book their tickets for the next year's dinner as soon as they had attended this one. They ran until 2004.<br /></td>
+        </tr>
+        <tr>
+          <td><strong>1988</strong></td>
+          <td>-</td>
+          <td>Mary Lou Schoenfeldt was appointed as Administrator. Although this was initially a small part time job with limited responsibilities, Mary Lou made the job, the Centre and indeed music in Townsville, her own. She was a wonderful asset to the Centre and to the cultural life of the City until she had to retire due to ill health in 2003.<br /></td>
+        </tr>
+        <tr>
+          <td><strong>1990</strong></td>
+          <td>-</td>
+          <td>This year the first lunchtime concert was held at the PercTucker Gallery. The importance of providing performance opportunities for emerging musicians had become evident and from this time, for a number of years, one of the major activities of the Centre was the production and management of concerts.<br /></td>
+        </tr>
+        <tr>
+          <td><strong>1999</strong></td>
+          <td>-</td>
+          <td>Arts Queensland had been a major funder of Music Centre activities for a number of years but in this year they agree to provide funding on a three year basis. This made it much easier for the Centre to plan ahead and reduced the time that had to be spent on repetitive grant applications. This arrangement ran until 2004.<br /></td>
+        </tr>
+        <tr>
+          <td><strong>2003</strong></td>
+          <td>-</td>
+          <td>A spectacular variety concert was held at the Townsville Civic Theatre to celebrate twenty years of the Music Centre and as a tribute to May Lou.</td>
+        </tr>
+        <tr>
+          <td><strong>2005</strong></td>
+          <td>-</td>
+          <td>The Centre moved to offices in the Old Magistrate's Court.</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td colspan="3">More details of the period 1983 - 2008 can be found in the book, <br />
+            <strong>&quot;Townsville Community Music Centre: some memories of the first 25 years&quot; <br />
+              by Jean Dartnall.</strong> (Copies available from the Music Centre)</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td><strong>2012</strong></td>
+          <td>-</td>
+          <td>The Centre moved to its present office in the Townsville Civic Theatre building.</td>
+        </tr>
+      </tbody>
+    </table>
+  </center>
+</div>
